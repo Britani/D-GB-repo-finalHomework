1747b3a29503016074480d0258e93295a93c8683
Tatiana Stepanova <kappro12@yandex.ru>
2023-04-06 21:44:20 +0300

    Добавлена задача, написаны методы создания и печати массива (черновой вариант)

--- Task000/Program.cs
+++ Task000/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.WriteLine("Зададим длинну будущего массива");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] collection = new string[size]; 
+FillArray(collection);
+PrintArrey(collection);
+
+       void FillArray(string[] coll)
+       {
+           int length = coll.Length;
+           int index = 0;
+           while (index < length)
+           {
+               Console.WriteLine($"Введите элемент с индексом {index}");
+               coll[index] = Console.ReadLine();//new Random().Next(1, 10);
+               index++;
+           }
+       }
+
+       void PrintArrey(string[] col)
+
+       {
+
+         int count = col.Length;
+
+         int position = 0;
+
+         while (position < count)
+
+         {
+             Console.Write(col[position]);
+             position++;
+         }
+       }  
